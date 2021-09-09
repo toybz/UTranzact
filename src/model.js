1919c8318f4c4547092a2c0467cd360aa74a7794
Toyeeb <toyeeb@flutterwavego.com>
2021-09-09 14:02:20 +0100

    Create model.js

---
+++ src/model.js
@@ -0,0 +1,12 @@
+wallet = {
+  balance: -1098,
+  cardDetails: {
+    cvv: "992",
+    expirationDate: "09/20",
+    name: "Toyeebabdulrahmon",
+    number: "000000",
+  },
+  id: "0",
+  name: "Default Account",
+  user: "1",
+};
